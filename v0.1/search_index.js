7928341705a178e351f8d457b66e3228dbb5aac5
Documenter.jl <documenter@juliadocs.github.io>
2020-08-20 22:24:48 +0000

    build based on af4f818

---
+++ v0.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#BFloat.jl","page":"BFloat.jl","title":"BFloat.jl","text":"","category":"section"},{"location":"#Index","page":"BFloat.jl","title":"Index","text":"","category":"section"},{"location":"","page":"BFloat.jl","title":"BFloat.jl","text":"","category":"page"}]
+}
